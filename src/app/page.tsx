39d03798da19ad09e9541b0295b2ad5c20ee75db
Nuel-C <eniemmanuel.c@gmail.com>
2025-02-17 15:45:15 +0100

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,34 +1,34 @@
 "use client";
-import { Pacifico, Grey_Qo} from "next/font/google";
+import { Pacifico, Grey_Qo } from "next/font/google";
 import Headder from "./headder";
-import Item from "./item"
-import Footer from "./footer"
+import Item from "./item";
+import Footer from "./footer";
 import { useState, useRef, useEffect } from "react";
 import ItemDetails from "./itemDetails";
 
-
+// Google Fonts
 const pacifico = Pacifico({
   subsets: ['cyrillic'],
   weight: '400'
-})
+});
 
 const grey_qo = Grey_Qo({
   subsets: ['latin-ext'],
   weight: '400'
-})
-
+});
 
 export default function Home() {
-  const ref = useRef(null);
+  // Typing ref to HTMLDivElement or null
+  const ref = useRef<HTMLDivElement | null>(null);
   const [classIndex, setClassIndex] = useState(1);
   const [itemDetailsToggle, setItemDetailsToggle] = useState(false);
 
   useEffect(() => {
     const intervalId = setInterval(() => {
-      if(ref.current){
+      if (ref.current) {
         // Remove previous background class
         ref.current.classList.remove('backgroundImage1', 'backgroundImage2', 'backgroundImage3');
-  
+
         // Add the new background class based on classIndex
         if (classIndex === 1) {
           ref.current.classList.add('backgroundImage1');
@@ -47,40 +47,45 @@ export default function Home() {
   }, [classIndex]);
 
   const setDetailsToggle = () => {
-    setItemDetailsToggle(!itemDetailsToggle)
-  }
+    setItemDetailsToggle(!itemDetailsToggle);
+  };
 
   return (
     <main>
-      <ItemDetails toggle={itemDetailsToggle} setItemToggle={setDetailsToggle}/>
+      <ItemDetails toggle={itemDetailsToggle} setItemToggle={setDetailsToggle} />
 
       <div className='backgroundImage1 w-full h-screen p-5 flex flex-col justify-between' ref={ref}>
         <Headder />
         <div className="flex justify-center">
-          <text className={`${pacifico.className} bg-black text-white p-2 rounded`}>Explore collection <i className="fa fa-angle-double-down" aria-hidden="true"></i></text>
+          <text className={`${pacifico.className} bg-black text-white p-2 rounded`}>
+            Explore collection <i className="fa fa-angle-double-down" aria-hidden="true"></i>
+          </text>
         </div>
       </div>
 
       <div className="m-5 grid gap-4 grid-cols-2 md:grid-cols-4">
-        <Item text='black' setItemToggle={setDetailsToggle}/>
-        <Item text='black' setItemToggle={setDetailsToggle}/>
-        <Item text='black' setItemToggle={setDetailsToggle}/>
-        <Item text='black' setItemToggle={setDetailsToggle}/>
-        <Item text='black' setItemToggle={setDetailsToggle}/>
-        <Item text='black' setItemToggle={setDetailsToggle}/>
-        <Item text='black' setItemToggle={setDetailsToggle}/>
+        <Item text='black' setItemToggle={setDetailsToggle} />
+        <Item text='black' setItemToggle={setDetailsToggle} />
+        <Item text='black' setItemToggle={setDetailsToggle} />
+        <Item text='black' setItemToggle={setDetailsToggle} />
+        <Item text='black' setItemToggle={setDetailsToggle} />
+        <Item text='black' setItemToggle={setDetailsToggle} />
+        <Item text='black' setItemToggle={setDetailsToggle} />
       </div>
-    
+
       <div className="backgroundImage4 w-full h-screen p-5 flex flex-col justify-between">
         <div></div>
         <div className="grid">
-          <text className={`${grey_qo.className} bg-black text-white p-2 rounded justify-self-end`}>The new collection is <br/>the synthesis of the harmonious <br/>elements of Playful elegance <br/><br/>EXPLORE OUR NEW COLLECTION <i className="fa fa-angle-double-down" aria-hidden="true"></i></text>
+          <text className={`${grey_qo.className} bg-black text-white p-2 rounded justify-self-end`}>
+            The new collection is <br />
+            the synthesis of the harmonious <br />
+            elements of Playful elegance <br /><br />
+            EXPLORE OUR NEW COLLECTION <i className="fa fa-angle-double-down" aria-hidden="true"></i>
+          </text>
         </div>
       </div>
 
-      <div>
-        <Footer/>
-      </div>
+      <Footer />
     </main>
   );
 }
